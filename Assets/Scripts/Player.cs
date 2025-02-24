c8a92c5e03a0d67669884a8fac0854f7edddb42a
Rodrigo Sonego <rodrigoasonego@gmail.com>
2025-02-24 11:45:22 -0300

    feature: camera control

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] Camera cam;
+    [SerializeField] float sensitivity = 2f;
+
     [SerializeField] float moveSpeed = 5.0f;
 
     Rigidbody rb;
@@ -17,8 +19,13 @@ public class Player : MonoBehaviour
     {
         moveAction = InputSystem.actions.FindAction("Move");
         lookAction = InputSystem.actions.FindAction("Look");
+        InputSystem.actions.FindAction("Pause").performed += ToggleMouseLock;
+
         rb = GetComponent<Rigidbody>();
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
+
 
     void FixedUpdate()
     {
@@ -27,13 +34,32 @@ public class Player : MonoBehaviour
         // X = camera axis Y
         // Y = camera axix X
         Vector2 lookValue = lookAction.ReadValue<Vector2>();
+        print(lookValue);
+
 
         Move(moveValue);
+        UpdateCamera(lookValue);
     }
 
     void Move(Vector2 moveValue)
     {
         Vector3 newPosition = rb.position + (new Vector3(moveValue.x, 0, moveValue.y) * moveSpeed * Time.deltaTime);
         rb.MovePosition(newPosition);
+    }
+
+    void UpdateCamera(Vector2 offset)
+    {
+        Vector3 camEuler = cam.transform.rotation.eulerAngles;
+        float xRotation = camEuler.x - (offset.y * sensitivity);
+
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        Vector3 bodyEuler = transform.rotation.eulerAngles;
+        transform.localRotation = Quaternion.Euler(0, bodyEuler.y + offset.x, 0);
+    }
+
+    private void ToggleMouseLock(InputAction.CallbackContext context)
+    {
+        Cursor.lockState = Cursor.lockState == CursorLockMode.Locked ? CursorLockMode.None : CursorLockMode.Locked;
     }
 }
