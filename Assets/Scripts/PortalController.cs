a88245deb4c2e55c08d0c8a2ddd6f76153816bb5
Rodrigo Sonego <rodrigoasonego@gmail.com>
2025-02-24 18:26:56 -0300

    feature: distancing camera based on player distance

--- Assets/Scripts/PortalController.cs
+++ Assets/Scripts/PortalController.cs
@@ -4,10 +4,28 @@ public class PortalController : MonoBehaviour
 {
     [SerializeField] Transform portal;
     [SerializeField] Camera portalCamera;
+    Vector3 portalCamStartingPos;
+    [SerializeField] float maxCameraDistance = 3;
 
     [SerializeField] Camera playerCamera;
 
+    private void Start()
+    {
+        portalCamStartingPos = portalCamera.transform.position;
+    }
+
     private void Update()
+    {
+        RotateCameraWithPlayer();
+
+        float distance = Vector3.Distance(playerCamera.transform.position, portal.transform.position);
+        distance = Mathf.Clamp(distance, 0, maxCameraDistance);
+
+        Vector3 offset = portalCamera.transform.forward * distance;
+        portalCamera.transform.position = portalCamStartingPos - offset;
+    }
+
+    private void RotateCameraWithPlayer()
     {
         float offsetX = portal.position.x - playerCamera.transform.position.x;
         float offsetZ = portal.position.z - playerCamera.transform.position.z;
