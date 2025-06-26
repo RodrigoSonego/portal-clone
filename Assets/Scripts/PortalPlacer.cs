b08b78ecca9aa1da05aa2773bdfce839dca336ed
Rodrigo Sonego <rodrigoasonego@gmail.com>
2025-06-26 12:08:26 -0300

    update: refactor and add comments to PortalPlacer

--- Assets/Scripts/PortalPlacer.cs
+++ Assets/Scripts/PortalPlacer.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class PortalPlacer : MonoBehaviour
 {
@@ -9,8 +10,12 @@ public class PortalPlacer : MonoBehaviour
 
     [SerializeField] private float offsetFromWall = 0.06f;
     
-    // TODO: Name this decently
-    [SerializeField] LayerMask layerMask;
+    [Space]
+    [SerializeField] private float verticalTestDistance = 1.2f;
+    [SerializeField] private float horizontalTestDistance = 0.9f;
+    [Space]
+    [SerializeField] LayerMask positionCorrectionMask;
+    
     void Start()
     {
         portal1.gameObject.SetActive(false);
@@ -26,9 +31,6 @@ public class PortalPlacer : MonoBehaviour
 
         if (hasHit && hit.collider.CompareTag("Wall"))
         {  
-            // hit.normal is negative, since we want the portal to point toward the wall
-
-            
             PositionAndRotatePortal(hit.point, hit.normal, portal);
             portal.SetWallCollider(hit.collider);
         }
@@ -36,6 +38,7 @@ public class PortalPlacer : MonoBehaviour
 
     void PositionAndRotatePortal(Vector3 position, Vector3 wallNormal, Portal portal)
     {
+        // hit.normal is negative, since we want the portal to point toward the wall
         Quaternion portalRotation = Quaternion.LookRotation(-wallNormal, Vector3.up);
         
         portal.transform.position = position + (wallNormal * offsetFromWall);
@@ -48,67 +51,80 @@ public class PortalPlacer : MonoBehaviour
         portal.isPlaced = true;
     }
 
+    // Fix positioning on intersections of terrain to no not go through walls, ceiling or floor 
     void FixPositioningOnIntersection(Portal portal)
     {
-        Vector3[] testDirections =
+        IntersectionTest[] tests =
         {
-            portal.transform.up,
-            -portal.transform.up,
-            portal.transform.right,
-            -portal.transform.right
+            new (portal.transform.up, verticalTestDistance),
+            new (-portal.transform.up, verticalTestDistance),
+            new (portal.transform.right, horizontalTestDistance),
+            new (-portal.transform.right, horizontalTestDistance),
         };
 
-        float[] testDistances = { 1.2f, 1.2f, 0.9f, 0.9f};
-
-        for (int i = 0; i < testDirections.Length; i++)
+        for (int i = 0; i < tests.Length; i++)
         {
             RaycastHit hit;
-            bool hasHit = Physics.Raycast(portal.transform.position, testDirections[i], out hit, testDistances[i], layerMask);
+            bool hasHit = Physics.Raycast(portal.transform.position, tests[i].Direction, out hit, tests[i].Distance, positionCorrectionMask);
             if (hasHit)
             {
-                // Debug.DrawRay(position, testDirections[i] * testDistances[i], Color.red, 5f);
-                float offset = testDistances[i] - hit.distance;
-                Vector3 newOffset = -offset * testDirections[i];
+                float offset = tests[i].Distance - hit.distance;
+                Vector3 newOffset = -offset * tests[i].Direction;
                 portal.transform.Translate(newOffset, Space.World);
-                // for (int j = 0; j < 4; j++)
-                // {
-                //     Debug.DrawLine(newOffset, testDirections[j] * testDistances[j], Color.yellow, 5f);
-                // }
             }
 
         }
     }
-
-    // TODO: Unfuck this and ammend commit
+    
+    // Fix positioning on edges/overhangs of terrain
+    // Defines a point behind each up-most, right-most, down-most and left-most point in the quad
+    // From there if it doesn't find a wall, shoots a ray on the opposite direction to find the distance to the
+    //wall and reposition the portal
     void FixOverhangPositioning(Portal portal)
     {
-        Vector3[] testDirections =
+        OverhangTest[] tests =
         {
-            -portal.transform.up,
-            portal.transform.up,
-            -portal.transform.right,
-            portal.transform.right,
+            new (portal.transform.up, new Vector3(0, -1.2f, 0.1f)),
+            new (-portal.transform.up, new Vector3(0, 1.2f, 0.1f)),
+            new (portal.transform.right, new Vector3(-0.9f, 0, 0.1f)),
+            new (-portal.transform.right, new Vector3(0.9f, 0, 0.1f)),
         };
-
-        Vector3[] testPoints =
+        
+        for (int i = 0; i < tests.Length; i++)
         {
-            new (0, 1.2f, 0.1f),
-            new (0, -1.2f, 0.1f),
-            new (0.9f, 0, 0.1f),
-            new (-0.9f, 0, 0.1f)
-        };
+            Vector3 rayPos = portal.transform.TransformPoint(tests[i].Point);
 
-        for (int i = 0; i < testDirections.Length; i++)
-        {
-            Vector3 rayPos = portal.transform.TransformPoint(testPoints[i]);
-
-            if (Physics.CheckSphere(rayPos, 0.2f, layerMask)) { continue; }
+            if (Physics.CheckSphere(rayPos, 0.2f, positionCorrectionMask)) { continue; }
             
-            if (Physics.Raycast(rayPos, testDirections[i], out RaycastHit hit, 2, layerMask))
+            if (Physics.Raycast(rayPos, tests[i].Direction, out RaycastHit hit, 2, positionCorrectionMask))
             {
                 Vector3 offset = hit.point - rayPos;
-                portal.transform.Translate(Vector3.Scale(-offset, testDirections[i]), Space.World);
+                portal.transform.Translate(Vector3.Scale(-offset, tests[i].Direction), Space.World);
             }
+        }
+    }
+
+    struct IntersectionTest
+    {
+        public Vector3 Direction;
+        public float Distance;
+
+        public IntersectionTest(Vector3 direction, float distance)
+        {
+            Direction = direction;
+            Distance = distance;
+        }
+    }
+
+    struct OverhangTest
+    {
+        public Vector3 Direction;
+        public Vector3 Point;
+
+        public OverhangTest(Vector3 direction, Vector3 point)
+        {
+            Direction = direction;
+            Point = point;
         }
     }
 }
